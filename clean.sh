2c3504d5a9af0d735fcb5c2ecb2c6077fe5e7075
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-30 12:50:16 +0200

    Works, no refs in Intro though

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm *.toc
+rm *.aux
+rm *.bbl
+rm *.blg
+rm *.log
+rm *.out
+rm *.pdf
--- merge_bibliographies.sh
+++ merge_bibliographies.sh
@@ -2,7 +2,7 @@
 
 bibtool -s -d \
   thesis_introduction/introduction.bib \
-  babette_article/article/article.bib \
+  babette_article/article/babette_article.bib \
   pirouette_article/article.bib \
   razzo_article/article/article.bib \
   thesis_synthesis/synthesis.bib \
