a8ede140b64538ff38fc26a790183f1e769d5bae
richelbilderbeek <richel@richelbilderbeek.nl>
2020-05-22 08:35:18 +0200

    Add 'push_all_githubs.sh'

---
+++ push_all_githubs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for folder in $(ls -d */ | egrep -v "(Urho3D|mycroft|\.Rcheck/)")
+do
+
+  cd $folder
+
+  if [ -d ".git" ]
+  then
+    echo "git folder: "$folder
+    git add --all :/
+    git commit -m "EOD"
+    git pull
+    git push
+  fi
+
+  cd ..
+done
