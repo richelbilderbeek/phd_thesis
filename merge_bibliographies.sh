d59f060b96663292073944e346c1c2a529d8b705
richelbilderbeek <richel@richelbilderbeek.nl>
2020-07-29 09:14:39 +0200

    Better bibliography merge

--- merge_bibliographies.sh
+++ merge_bibliographies.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-bibtool -s -d \
-  thesis_introduction/introduction.bib \
-  babette_article/article/babette_article.bib \
-  pirouette_article/pirouette_article.bib \
-  razzo_article/article/article.bib \
-  thesis_synthesis/synthesis.bib \
-  -o dissertation.bib
+# Use all bibs in subfolders
+bibtool -s -d $(find . | egrep "./.*/.*bib$") -o dissertation.bib
+
+#bibtool -s -d \
+#  thesis_introduction/introduction.bib \
+#  babette_article/article/babette_article.bib \
+#  pirouette_article/pirouette_article.bib \
+#  razzo_article/article/article.bib \
+#  thesis_synthesis/synthesis.bib \
+#  -o dissertation.bib
