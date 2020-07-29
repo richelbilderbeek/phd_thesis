961f050ef6f5cd6938a84868381a6879acab06a5
richelbilderbeek <richel@richelbilderbeek.nl>
2020-07-29 08:34:07 +0200

    'merge_bibliographies.sh' uses 'pirouette_article/pirouette_article.bib'

--- merge_bibliographies.sh
+++ merge_bibliographies.sh
@@ -3,7 +3,7 @@
 bibtool -s -d \
   thesis_introduction/introduction.bib \
   babette_article/article/babette_article.bib \
-  pirouette_article/article.bib \
+  pirouette_article/pirouette_article.bib \
   razzo_article/article/article.bib \
   thesis_synthesis/synthesis.bib \
   -o dissertation.bib
